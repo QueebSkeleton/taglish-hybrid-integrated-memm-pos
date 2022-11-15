54c2b97f5180d383a3709d99bdeea03407915898
Rian Carlo Reyes <reyesriancarlo@gmail.com>
2022-11-15 16:38:23 +0800

    Lay down website pages

--- tagger/urls.py
+++ tagger/urls.py
@@ -6,4 +6,9 @@ app_name = "tagger"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("cite", views.cite, name="cite"),
+    path("annotator", views.annotator, name="annotator"),
+    path("online-model", views.online_model, name="online_model"),
+    path("contact", views.contact, name="contact"),
 ]
--- tagger/views.py
+++ tagger/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "tagger/index.html", {})
+
+
+def about(request):
+    return render(request, "tagger/about.html", {})
+
+
+def cite(request):
+    return render(request, "tagger/cite.html", {})
+
+
+def annotator(request):
+    return render(request, "tagger/annotator.html", {})
+
+
+def online_model(request):
+    return render(request, "tagger/online_model.html", {})
+
+
+def contact(request):
+    return render(request, "tagger/contact.html", {})

2df5543c7317b6435a15d81bae724af964ce8b89
Rian Carlo Reyes <reyesriancarlo@gmail.com>
2022-11-26 17:07:12 +0800

    Add annotator functionality with csv output

--- tagger/spas/annotator/src/App.js
+++ tagger/spas/annotator/src/App.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 
 import { Badge, Button, ButtonGroup, Col, Form, Row, Table } from 'react-bootstrap';
 
+
 const POS_TAGS = [
   { tag: "ADJ", verbose: "Adjective" },
   { tag: "ADV", verbose: "Adverb" },
   { tag: "INTJ", verbose: "Interjection" },
   { tag: "NOUN", verbose: "Noun" },
   { tag: "PROPN", verbose: "Proper Noun" },
+  { tag: "VERB", verbose: "Verb" },
   { tag: "ADP", verbose: "Adposition" },
   { tag: "AUX", verbose: "Auxiliary" },
   { tag: "CCONJ", verbose: "Coordinating Conjunction" },
@@ -22,31 +24,41 @@ const POS_TAGS = [
   { tag: "SYM", verbose: "Symbol" },
   { tag: "X", verbose: "Other" }];
 
+const VALID_TOKEN_REGEX = /^\S+$/;
+
 const InputSentenceForm = (props) => {
+  const [id, setId] = useState("");
+  const [language, setLanguage] = useState("TAGLISH");
   const [sentence, setSentence] = useState("");
-
-  const onStartClick = e => {
-    let tokens;
-    const tokenizeRequest = async () => {
-      const response = await axios.get(
-        `/tokenize/`, { params: { sentence: sentence } });
-      tokens = response.data.tokens;
-    };
-
-    tokenizeRequest().then(() => {
-      props.initializeCallback(tokens);
-    });
-  };
 
   // TODO: Don't allow to proceed when sentence is improper/empty.
   return (
     <div className={props.className}>
       <h1 className="h5">Input sentence to annotate</h1>
+      <Row className="mb-3">
+        <Col md={6}>
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="text" size="sm" value={id}
+            onChange={e => setId(e.target.value)}
+            disabled={props.disabled} />
+        </Col>
+        <Col md={6}>
+          <Form.Label>Language</Form.Label>
+          <Form.Select size="sm" value={language}
+            onChange={e => setLanguage(e.target.value)}
+            disabled={props.disabled}>
+            <option value="TAGLISH">Taglish</option>
+            <option value="ENG">English</option>
+            <option value="FIL">Tagalog</option>
+          </Form.Select>
+        </Col>
+      </Row>
       <Form.Control as="textarea" rows={3} value={sentence}
         onChange={event => setSentence(event.target.value)}
-        disabled={props.isAnnotating} className="mb-2"></Form.Control>
-      <Button size="sm" variant="outline-primary" onClick={onStartClick}
-        disabled={props.isAnnotating}>
+        disabled={props.disabled} className="mb-2"></Form.Control>
+      <Button size="sm" variant="outline-primary"
+        onClick={e => props.initializeCallback({ id, language, raw: sentence })}
+        disabled={props.disabled || !id || !language || !sentence}>
         <i className="fa-solid fa-marker"></i> Start annotating
       </Button>
     </div>
@@ -60,22 +72,35 @@ const SentenceAnnotationPanel = (props) => {
         <Row className="mb-3">
           <Col xs={8} className="border-end">
             <SentenceDisplayPanel tokens={props.tokens}
-              highlightIndex={props.annotateIndex} />
+              annotateIndex={props.annotateIndex}
+              setAnnotateIndex={props.setAnnotateIndex} />
           </Col>
           <Col xs={4}>
-            <AddTokenPanel />
+            <AddTokenPanel
+              insertBeforeCurrentCallback={props.insertBeforeCurrentCallback}
+              insertAfterCurrentCallback={props.insertAfterCurrentCallback}
+              disabled={props.annotateIndex >= props.tokens.length} />
           </Col>
         </Row>
         <Row className="pt-3 border-top mb-3">
           <Col xs={4} className="border-end">
-            <EditTokenPanel />
+            <EditTokenPanel
+              currentToken={props.annotateIndex < props.tokens.length ?
+                props.tokens[props.annotateIndex].token : ""}
+              disabled={props.annotateIndex >= props.tokens.length}
+              removeDisabled={props.tokens.length === 1}
+              editCurrentTokenCallback={props.editCurrentTokenCallback}
+              removeCurrentTokenCallback={props.removeCurrentTokenCallback} />
           </Col>
           <Col xs={8}>
-            <TagButtonsPanel annotateCallback={props.annotateCallback} />
+            <TagButtonsPanel disabled={props.annotateIndex >= props.tokens.length}
+              annotateCallback={props.annotateCallback} />
           </Col>
         </Row>
         <div className="border-top pt-3">
-          <Button size="sm" variant="outline-primary">
+          <Button size="sm" variant="outline-primary"
+            onClick={props.submitCallback}
+            disabled={!props.canSubmit}>
             <i className="fa-solid fa-hand-point-up"></i> Submit to session
           </Button>
         </div>
@@ -88,29 +113,34 @@ const SentenceAnnotationPanel = (props) => {
 };
 
 const SentenceDisplayPanel = (props) => {
+  const onTokenClick = (index) => {
+    props.setAnnotateIndex(index);
+  };
+
   return (
     <>
       <h1 className="h5">Your sentence</h1>
       <div className="mb-3">
         {props.tokens?.map(({ token, tag }, index) => {
           if (tag) {
-            if (props.highlightIndex === index)
-              return <span className="border-bottom border-primary border-2
-              pb-1 me-1 mb-1">
-                <AnnotatedTokenBadge key={index}
-                  tag={tag} token={token}
-                  className="me-1 mb-1" />
+            if (props.annotateIndex === index)
+              return <span key={index}
+                className="border-bottom border-primary
+                  border-2 pb-1 me-1 mb-1"
+                onClick={() => onTokenClick(index)}>
+                <AnnotatedTokenBadge tag={tag} token={token} />
               </span>;
 
             else
               return <AnnotatedTokenBadge key={index}
-                tag={tag} token={token}
-                className="me-1 mb-1" />;
+                onClick={() => onTokenClick(index)}
+                tag={tag} token={token} className="me-1 mb-1" />;
           }
           else
             return <span key={index} className={`me-1 mb-1
-              ${props.highlightIndex === index ?
-                'border-bottom border-primary border-2 pb-1' : null}`}>
+              ${props.annotateIndex === index ?
+                'border-bottom border-primary border-2 pb-1' : null}`}
+              onClick={() => onTokenClick(index)}>
               {token}
             </span>;
         })}
@@ -126,7 +156,8 @@ const SentenceDisplayPanel = (props) => {
 const AnnotatedTokenBadge = (props) => {
   return (
     <Badge bg={`pos-${props.tag.toLowerCase()}`}
-      className={props.className}>
+      className={props.className}
+      onClick={props.onClick}>
       {props.token} | {props.tag}
     </Badge>
   );
@@ -135,18 +166,34 @@ const AnnotatedTokenBadge = (props) => {
 const AddTokenPanel = (props) => {
   const [token, setToken] = useState("");
 
+  const onTokenChange = (e) => {
+    const value = e.target.value;
+    if (VALID_TOKEN_REGEX.test(value))
+      setToken(value);
+  };
+
   return (
     <>
       <h1 className="h5">Add a token</h1>
       <Form.Control type="text" size="sm" value={token}
-        onChange={e => setToken(e.target.value)} />
+        onChange={onTokenChange} disabled={props.disabled} />
       <Form.Text>Insert relative to current:</Form.Text>
       <div>
         <ButtonGroup>
-          <Button size="sm" variant="outline-primary">
+          <Button size="sm" variant="outline-primary"
+            onClick={e => {
+              props.insertBeforeCurrentCallback(token);
+              setToken("");
+            }}
+            disabled={props.disabled || token === ""}>
             <i className="fa-solid fa-arrow-left"></i> Before
           </Button>
-          <Button size="sm" variant="outline-primary">
+          <Button size="sm" variant="outline-primary"
+            onClick={e => {
+              props.insertAfterCurrentCallback(token);
+              setToken("");
+            }}
+            disabled={props.disabled || token === ""}>
             After <i className="fa-solid fa-arrow-right"></i>
           </Button>
         </ButtonGroup>
@@ -156,16 +203,22 @@ const AddTokenPanel = (props) => {
 };
 
 const EditTokenPanel = (props) => {
-  const [token, setToken] = useState("");
+  const onTokenChange = e => {
+    const value = e.target.value;
+    if (VALID_TOKEN_REGEX.test(value))
+      props.editCurrentTokenCallback(value);
+  };
 
   return (
     <>
       <h1 className="h5">Current token</h1>
-      <Form.Control type="text" size="sm" value={token}
-        onChange={e => setToken(e.target.value)} />
+      <Form.Control type="text" size="sm" value={props.currentToken}
+        onChange={onTokenChange} disabled={props.disabled} />
       <Form.Text>Change as necessary, or delete below.</Form.Text>
       <div>
-        <Button size="sm" variant="outline-danger">
+        <Button size="sm" variant="outline-danger"
+          onClick={e => props.removeCurrentTokenCallback()}
+          disabled={props.removeDisabled || props.disabled}>
           <i className="fa-solid fa-trash"></i> Remove token
         </Button>
       </div>
@@ -180,7 +233,8 @@ const TagButtonsPanel = (props) => {
       {POS_TAGS.map(({ tag, verbose }) =>
         <button key={tag} type="button"
           className={`btn btn-sm btn-pos-${tag.toLowerCase()} me-1 mb-1`}
-          onClick={e => props.annotateCallback(tag)}>
+          onClick={e => props.annotateCallback(tag)}
+          disabled={props.disabled}>
           {verbose} | {tag}
         </button>)}
     </div>
@@ -202,31 +256,107 @@ const SummaryTable = () => {
 };
 
 const App = () => {
+  // Input details
+  const [sentenceInput, setSentenceInput] = useState({
+    id: null, raw: null, language: null
+  })
+  // Flag if the form should display the annotation form or not.
   const [isAnnotating, setAnnotating] = useState(false);
+  // Current tokens with their tags.
   const [tokens, setTokens] = useState(null);
+  // Current token in "tokens" state being annotated.
   const [annotateIndex, setAnnotateIndex] = useState(-1);
+  // Flag if the current sentence can be submitted now (submit button disabled?)
+  // Only true if all tokens are annotated.
+  const [canSubmit, setCanSubmit] = useState(false);
 
-  const initializeWithTokens = (tokens) => {
-    setAnnotating(true);
-    setAnnotateIndex(0);
-    setTokens(tokens.map((token) => { return { token, tag: null }; }));
+  // Initializes to annotation mode with the inputted sentence
+  const initializeWithSentence = (sentenceInput) => {
+    setSentenceInput(sentenceInput);
+    const tokenizeThenInitialize = async () => {
+      // Hit API to tokenize the sentence, get response
+      const response = await axios.get(
+        `/tokenize/`, { params: { sentence: sentenceInput.raw } });
+      // TODO: Handle HTTP error codes!
+      setTokens(response.data.tokens.map((token) => ({ token, tag: null })));
+      setAnnotating(true);
+      setAnnotateIndex(0);
+    };
+
+    tokenizeThenInitialize();
   };
 
+  // Annotate the token at annotateIndex with the given tag.
   const annotateCurrentWithTag = (tag) => {
     let newTokens = [...tokens];
     newTokens[annotateIndex].tag = tag;
     setTokens(newTokens);
     setAnnotateIndex(annotateIndex + 1);
+
+    // Check if annotation can be submitted
+    // by finding an unannotated token. If there is, set flag to false
+    const unannotatedToken = newTokens.find(
+      (tokenAnnotation) => !Boolean(tokenAnnotation.tag));
+    if (unannotatedToken) setCanSubmit(false);
+    else setCanSubmit(true);
+  };
+
+  // Inserts a new token at the specified index.
+  const insertTokenAtIndex = (token, index) => {
+    let newTokens = [...tokens];
+    newTokens.splice(index, 0, { token, tag: null });
+    setTokens(newTokens);
+    setAnnotateIndex(index);
+    setCanSubmit(false);
+  };
+
+  // Edits token at specified index.
+  const editTokenAtIndex = (token, index) => {
+    let newTokens = [...tokens];
+    newTokens[index].token = token;
+    setTokens(newTokens);
+  };
+
+  // Removes token at specified index.
+  const removeTokenAtIndex = (index) => {
+    let newTokens = [...tokens];
+    newTokens.splice(index, 1);
+    setTokens(newTokens);
+  };
+
+  // Submits the annotated sentence to session.
+  const onSubmitClick = (e) => {
+    const submitRequest = async () => {
+      await axios.post('/save-to-session/', {
+        ...sentenceInput,
+        annotated: tokens.reduce((annotated, token) =>
+          annotated + `<${token.tag} ${token.token}> `, "").trim()
+      });
+    };
+
+    submitRequest().then(() => { window.location.reload(); });
   };
 
   return (
     <>
       <InputSentenceForm className="border-top pt-3 mb-3"
-        isAnnotating={isAnnotating} initializeCallback={initializeWithTokens} />
+        initializeCallback={initializeWithSentence}
+        disabled={isAnnotating} />
       {isAnnotating ?
         <SentenceAnnotationPanel className="border-top pt-3"
-          tokens={tokens} annotateIndex={annotateIndex}
-          annotateCallback={annotateCurrentWithTag} /> : null}
+          tokens={tokens}
+          annotateIndex={annotateIndex} setAnnotateIndex={setAnnotateIndex}
+          annotateCallback={annotateCurrentWithTag}
+          insertBeforeCurrentCallback={
+            (token) => insertTokenAtIndex(token, annotateIndex)}
+          insertAfterCurrentCallback={
+            (token) => insertTokenAtIndex(token, annotateIndex + 1)}
+          editCurrentTokenCallback={
+            (token) => editTokenAtIndex(token, annotateIndex)}
+          removeCurrentTokenCallback={
+            () => removeTokenAtIndex(annotateIndex)}
+          canSubmit={canSubmit}
+          submitCallback={onSubmitClick} /> : null}
     </>
   );
 };
--- tagger/views.py
+++ tagger/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core.paginator import Paginator
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+import csv
 
 from nltk.tokenize import word_tokenize
 
@@ -22,13 +27,49 @@ def cite(request):
 
 
 def annotator(request):
-    return render(request, "tagger/annotator.html", {})
+    session_sentences = request.session.get('sentences', {}).values()
+    return render(request, "tagger/annotator.html",
+                  {'session_sentences': session_sentences,
+                   'session_sentences_len': len(session_sentences)})
 
 
 def tokenize(request):
     input_sentence = request.GET.get('sentence')
     tokens = word_tokenize(input_sentence)
     return JsonResponse({'tokens': tokens})
+
+
+@csrf_exempt
+def add_sentence_to_session(request):
+    request_body = json.loads(request.body)
+
+    session_sentences = request.session.get('sentences', {})
+    session_sentences[request_body['id']] = request_body
+    request.session['sentences'] = session_sentences
+
+    return HttpResponse(status=204)
+
+
+def remove_sentences_from_session(request):
+    if 'sentences' in request.session:
+        del request.session['sentences']
+    return HttpResponseRedirect(reverse("tagger:annotator"))
+
+
+def get_session_sentences_as_csv(request):
+    session_sentences = request.session.get('sentences', {})
+
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="dataset.csv"'})
+    csv_writer = csv.writer(response)
+    csv_writer.writerow(['id', 'language', 'raw', 'annotated'])
+    
+    for sentence in session_sentences.values():
+        csv_writer.writerow([sentence['id'], sentence['language'],
+                             sentence['raw'], sentence['annotated']])
+    
+    return response
 
 
 def browse_dataset(request):
@@ -44,9 +85,19 @@ def browse_dataset(request):
 
 
 def fetch_annotated_sentence(request, id):
-    sentence = AnnotatedSentence.objects.get(pk=id)
+    # Determines whether the sentence is from database table or session storage
+    from_storage = request.GET.get('from', 'session')
+
+    if from_storage == "database":
+        annotated_sentence = AnnotatedSentence.objects.get(pk=id).annotated
+    elif from_storage == "session":
+        annotated_sentence = request.session['sentences'][id]['annotated']
+
+    if not annotated_sentence:
+        return HttpResponse(status=404)
+
     # Transform annotation into a list of 2-tuples
-    annotation_as_list = transform_into_listtuple(sentence.annotated)
+    annotation_as_list = transform_into_listtuple(annotated_sentence)
     # Transform into a JSON
     return JsonResponse({'annotation':
                          [{'tag': annotated_token[0], 'token': annotated_token[1]}
